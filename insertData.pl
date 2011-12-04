b2f7eae7b2475e5be7f393d9885b9bdb9e5d7114
Hanna Yoo <amor0624.yoo@gmail.com>
2011-12-03 18:43:11 -0800

    corrected one defect from Kim's code=P

--- insertData.pl
+++ insertData.pl
@@ -1,5 +1,9 @@
-:- [tokenizer].
 
+
+split_string(Input,Seperator,Output) :-
+	toLowerCase(Input, LCInput),
+	string_tokens(LCInput, Seperator, SplitedString),
+	codeListToChars(SplitedString, Output).
 
 string_tokens(Cs, StpS, Ts) :- phrase(tokens(Cs, StpS, []), Ts).
 
@@ -10,3 +14,13 @@ tokens([C|Cs], StpS, Ts) -->
 
 token([]) --> [].
 token([T|Ts]) --> { reverse([T|Ts], Token) }, [Token].
+
+codeListToChars([],[]) :- !.
+codeListToChars([H|T],[X|Xs]) :- 
+	read_from_chars(H,X),
+	codeListToChars(T,Xs).
+	
+toLowerCase([],[]).
+toLowerCase([H|T],[H1|T1]) :- 
+	65 =< H, H =< 90, !, H1 is H + 32, toLowerCase(T,T1).
+toLowerCase([H1|T],[H1|T1]) :- toLowerCase(T,T1).
