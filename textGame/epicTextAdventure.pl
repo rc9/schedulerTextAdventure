3343db805b04ddc03b7ffb47dbc5c522460aad2d
Riley Chang <r9chang@gmail.com>
2011-12-04 11:33:59 -0800

    tell it what to do works :D

--- textGame/epicTextAdventure.pl
+++ textGame/epicTextAdventure.pl
@@ -1,9 +1,17 @@
-:-[tokenizer], [write], [grammar].
+:-[tokenizer].
+:-[write].
+:-[grammar].
 
-play :- write('Welcome!'), nl, inner_play, !, loop_thru_game.
-loop_thru_game :- repeat, check_over, read_atomics(Sent), phrase(C,O,Sent,[]), C=..O, do(C), fail.
+play :- write('Welcome!'), nl, !, loop_thru_game.
+loop_thru_game :- repeat, read_atomics(Sent), phrase(L,Sent,[]), write(L), nl, C=..L, write(C), nl, do(C), (check_over; C==quit).
+
+do(X) :- X, !.
 
 check_over :- check_won, !.
 check_over :- check_lost.
+
+% need some conditions.
+check_won :- fail.
+check_lost :- fail.
 
 
