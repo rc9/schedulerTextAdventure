014fd92480210f505f1baf3fd78e8ff99eb0517c
Rob MacEachern <rob@robmaceachern.com>
2011-12-04 09:01:33 -0800

    first bit of the world

---
+++ textGame/world.pl
@@ -0,0 +1,25 @@
+place(house).
+place(bar).
+place(lab).
+place(jail).
+place(library).
+
+at(mom, house).
+at(brother, house).
+at(bartender, bar).
+at(policeman, bar).
+at(ta, lab).
+at(jailer, jail).
+at(prisoner, jail). %a random prisoner also in jail
+at(kim, library).
+at(librarian, library).
+
+person(mom).
+person(brother).
+person(bartender).
+person(policeman).
+person(ta).
+person(jailer).
+person(prisoner).
+person(kim).
+person(librarian).
