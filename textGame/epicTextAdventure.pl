dfb94cd4411805007a739ad3149da6fbd788a66f
Riley Chang <r9chang@gmail.com>
2011-12-04 19:22:12 -0800

    merging

--- textGame/epicTextAdventure.pl
+++ textGame/epicTextAdventure.pl
@@ -3,7 +3,7 @@
 :-[grammar].
 
 play :- write('Welcome!'), nl, inner_play, !, loop_thru_game.
-loop_thru_game :- repeat, execute(C), do(C), ((C==quit/*, check_over*/), !).
+loop_thru_game :- repeat, execute(C), do(C), ((C==quit; check_over), retract(here(_)), !).
 
 execute(C) :- read_atomics(Sent), phrase(L,Sent,[]), C=..L, !.
 execute(_) :- write('You talkin crazy boi/grl!'), nl, fail.
